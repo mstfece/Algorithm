66a029c42b4f091660a6d7e21053bc573ab3bd99
Mustafa ECE <82402952+mstfece@users.noreply.github.com>
2023-08-17 23:02:28 +0300

    Time: 67 ms (41.48%) | Memory: 44.3 MB (36.11%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.js
@@ -0,0 +1,20 @@
+/*
+ * @param {string} s
+ * @return {number}
+ */
+
+var longestPalindrome = function (s) {
+    let obj = {};
+    let answer = 0;
+    for (let c of s) {
+        if (!obj[c]) {
+            obj[c] = 1;
+        }
+        else
+            obj[c]++;
+        if (obj[c] % 2 === 0) {
+            answer += 2;
+        }
+    }
+    return s.length > answer ? answer + 1 : answer;
+};
